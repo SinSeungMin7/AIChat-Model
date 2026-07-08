bccaa91d789fee29856889df2f2de2567c518fa5
SinSeungMin7 <min85112000@gmail.com>
2026-07-08 13:48:42 +0900

    생성자 추가

--- src/main/java/com/example/ai/chat/service/ChatRoomService.java
+++ src/main/java/com/example/ai/chat/service/ChatRoomService.java
@@ -1,6 +1,7 @@
 package com.example.ai.chat.service;
 
 import com.example.ai.chat.dto.ChatRoom;
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
@@ -8,6 +9,14 @@ import java.util.*;
 @Service
 public class ChatRoomService {  // 채팅방 관리 클래스 생성
     private final Map<String, ChatRoom> chatRooms = new HashMap<>();
+
+    public ChatRoomService(){
+        createRoom(
+                "group",
+                "프로젝트 단체채팅",
+                "GROUP"
+        );
+    }
 
     public ChatRoom createRoom(String roomId, String roomName, String roomType){
 
