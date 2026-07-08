7837ca8bd5ff3eb5989221ed6d2da13a14be4e2d
SinSeungMin7 <min85112000@gmail.com>
2026-07-08 12:46:47 +0900

    문단 정리

--- src/main/resources/static/chat.js
+++ src/main/resources/static/chat.js
@@ -42,10 +42,8 @@ let stompClient = null;
 
 // 채팅 팝업 열기 (팝업 열면 자동실행)
 function openChat(){
-
     document.getElementById("chatModal").style.display="block";
     loadUsers();
-
 }
 
 // 채팅 팝업 닫기
@@ -56,35 +54,27 @@ function closeChat() {
 
 // 단체 / 개인 채팅 전환
 function switchTab(type) {
-
     const group = document.getElementById("groupChat");
     const personal = document.getElementById("privateChat");
-
     const buttons = document.querySelectorAll(".tab button");
 
     buttons.forEach(btn => btn.classList.remove("active"));
 
     if (type === "group") {
-
         group.style.display = "block";
         personal.style.display = "none";
 
         buttons[0].classList.add("active");
-
     } else {
-
         group.style.display = "none";
         personal.style.display = "block";
 
         buttons[1].classList.add("active");
-
     }
-
 }
 
 // WebSocket 연결
 function connect() {
-
     const socket = new SockJS('/chat');
 
     stompClient = Stomp.over(socket);
@@ -100,34 +90,27 @@ function connect() {
 
 // 메시지 보내기
 function sendMessage() {
-
     const input = document.getElementById("message");
     const text = input.value;
     // 나중에는  const currentUser = loginUser; -> 로그인유저(변수명)로 교체
     const currentUser = "USER";
-
 
     if (text.trim() === "") {
         return;
     }
 
     stompClient.send("/app/chat.send", {}, JSON.stringify({
-
         sender: currentUser,
         message: text,
         //roomId: "group",
         roomId: currentRoom,  /* 단체 채팅이면  roomId = group, ex)김철수 를 누르면 roomId = 김철수 로 표시*/
         type: "TALK"
-
     }));
-
     input.value = "";
-
 }
 
 // 화면 출력
 function showMessage(message) {
-
     const area = document.getElementById("messageArea");
 
     area.innerHTML += `
@@ -135,12 +118,10 @@ function showMessage(message) {
             <b>${message.sender}</b> : ${message.message}
         </div>
     `;
-
 }
 
 // 페이지 로드 / enter키 눌럿을 때 바로 입력
 window.onload = function () {
-
     connect();
 
     const input = document.getElementById("message");
@@ -149,14 +130,11 @@ window.onload = function () {
         .addEventListener("click", sendMessage);
 
     input.addEventListener("keydown", function (event) {
-
         if (event.key === "Enter") {
             event.preventDefault();
             sendMessage();
         }
-
     });
-
 }
 
 /*
